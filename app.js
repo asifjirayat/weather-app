6ceaed0463c188adee2c53323165894b1e155c7e
Asif Jirayat <asifjirayat@gmail.com>
2025-08-16 19:42:22 +0530

    Attempted to load a local weather.json file for data, due to limited calls on weatherapi.com

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
 
 const app = express();
-
 const PORT = 3000;
 
+// Function to load weather data from JSON file
+const loadWeatherData = async () => {
+  console.log("Attempting to load weather data....");
+  try {
+    const data = await fs.readFile(
+      path.join(__dirname, "weather.json"),
+      "utf-8"
+    );
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error loading weather data:", error);
+    return {};
+  }
+};
+
+// Global variable to store weather data
+let weatherData = {};
+
+// IIFE to Load data when app starts
+(async () => {
+  weatherData = await loadWeatherData();
+  console.log(
+    `Loaded ${Object.keys(weatherData).length} cities from weather.json`
+  );
+})();
+
 app.get("/", (req, res) => {
-  res.send("Hello! Weather app is running...");
+  res.send("Hello! Weather app is running....");
 });
 
 app.listen(PORT, () => {
-  console.log("Wheather app is running on http://localhost:" + PORT);
+  console.log("Weather app is running on http://localhost:", PORT);
 });
